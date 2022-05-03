9e6b0b5f8b06808b3796c7ac6aa481354512c985
Ammar Ahmed <ahmedammar841@gmail.com>
2022-05-03 00:38:23 -0500

    Add base to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
 	site: "https://ahmedammar841.github.io",
+	base: '/Andir/',
 	integrations: [
 		// Enable Preact to support Preact JSX components.
 		preact(),
